8e252884d536cbd63a0c8c8dcae9d709bfa1f128
19carenso <maxcarenso@hotmail.com>
2024-10-04 14:51:34 +0200

    Initial commit for sharing

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(name='tempest',
+version='0.1',
+description='Tallying Engine for Multiscale Precipitation Events with Storm Tracking',
+url='#',
+author='Maxime Carenso',
+author_email='maxime.carenso@lmd.ipsl.fr',
+license='MIT',
+packages=['tempest'],
+install_requires=[],      
+zip_safe=False) 
+ 
